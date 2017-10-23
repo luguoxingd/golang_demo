3d8a6c64d43f6f1bbfa5a639262c4d5d52e2555d
luguoxing <luguoxing>
2017-10-23 16:13:22 +0800

    'web'

--- main.go
+++ main.go
@@ -1,4 +1,10 @@
 package main
-func main(){
-  println("hello")
+import "net/http"
+func main() {
+  http.HandleFunc("/", hello)
+  http.ListenAndServe(":8080", nil)
 }
+func hello(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("hello!"))
+}
+
